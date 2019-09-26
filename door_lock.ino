0bc5ad30619a49b82133b75fe9a3e6ea2bc567c1
tarunjain3 <48101442+tarunjain3@users.noreply.github.com>
2019-09-26 23:53:15 +0530

    Add files via upload

--- door_lock.ino
+++ door_lock.ino
@@ -13,34 +13,33 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 int count=0;
 RTC_DS1307 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
+int relay=15;
 void setup() {
   Serial.begin(115200);
   pinMode(2,OUTPUT);
-/*  if (! rtc.begin()) {
+  pinMode(relay,OUTPUT);
+ if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
-  }*/
- //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+ rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   //rtc.adjust(DateTime(2019, 8, 23, 2, 32, 00));
 
-   pinMode(2,OUTPUT);
-  Serial.begin(115200);   // Initiate a serial communication
+
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("take your card to the reader...");
 
- /* if (!EEPROM.begin(64000)) {
+  if (!EEPROM.begin(64000)) {
     Serial.println("Failed to initialise EEPROM");
     Serial.println("Restarting...");
     delay(1000);
     ESP.restart();
-  }*/
+  }
 
 }
 
 void loop() {
-   digitalWrite(2,HIGH);
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -67,51 +66,51 @@ void loop() {
   content.toUpperCase();
   if (content.substring(1) == "03 6F CD 73") //change here the UID of the card/cards that you want to give access
   {
+        digitalWrite(2,HIGH);
     Serial.println("Authorized access");
     EEPROM.writeString(count,+"A   time" ) ;
     Serial.println(EEPROM.readString(count));
     DateTime now = rtc.now();
-    Serial.print("year:");
+     Serial.print("Date : ");
+     Serial.print(now.day(), DEC);
+    Serial.print("/");
+     Serial.print(now.month(), DEC);
+    Serial.print("/");
    Serial.println(now.year(), DEC);
-    Serial.print("month:");
-    Serial.println(now.month(), DEC);
-    Serial.print("date:");
-    Serial.println(now.day(), DEC);
-    delay(2000);
      Serial.print("day:");
     Serial.println(daysOfTheWeek[now.dayOfTheWeek()]);
-     Serial.println("time");
+     Serial.print("time : ");
      Serial.print(now.hour(), DEC);
     Serial.print(':');
     Serial.print(now.minute(), DEC);
     Serial.print(':');
     Serial.println(now.second(), DEC);
-    digitalWrite(2,HIGH);
-    
+    digitalWrite(relay,HIGH);
+     Serial.println(" ");
   }
    else if (content.substring(1) == "EB 43 01 99") //change here the UID of the card/cards that you want to give access
   {
-    Serial.println("Authorized access at 2");
-     Serial.println("Authorized access");
-    EEPROM.writeString(count,"B   time" );
+        digitalWrite(2,HIGH);
+    Serial.println("Authorized access");
+    EEPROM.writeString(count,+"A   time" ) ;
     Serial.println(EEPROM.readString(count));
     DateTime now = rtc.now();
-    Serial.print("year:");
+     Serial.print("Date : ");
+     Serial.print(now.day(), DEC);
+    Serial.print("/");
+     Serial.print(now.month(), DEC);
+    Serial.print("/");
    Serial.println(now.year(), DEC);
-    Serial.print("month:");
-    Serial.println(now.month(), DEC);
-    Serial.print("date:");
-    Serial.println(now.day(), DEC);
-    delay(2000);
      Serial.print("day:");
     Serial.println(daysOfTheWeek[now.dayOfTheWeek()]);
-     Serial.println("time");
+     Serial.print("time : ");
      Serial.print(now.hour(), DEC);
     Serial.print(':');
     Serial.print(now.minute(), DEC);
     Serial.print(':');
     Serial.println(now.second(), DEC);
-    digitalWrite(2,HIGH);
+    digitalWrite(relay,HIGH);
+         Serial.println(" ");
   }
  else   {
     Serial.println(" Access denied");
